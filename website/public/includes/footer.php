a93dc522a75bf6d6d72c1016875e470865e4def1
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2022-11-09 10:23:11 +0300

    Add files via upload

---
+++ website/public/includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer">
+        <div class="container">
+            <p class="border-top mb-0 mt-4 pt-3 small">&copy; <script>document.write(new Date().getFullYear())</script> Rights Reserved. </p> 
+        </div>      
+</footer>
---
+++ website/public/includes/header.php
@@ -0,0 +1,56 @@
+
+    <!-- End Of First Navigation -->
+    <!-- Page Second Navigation -->
+
+    <!-- End Of Page Second Navigation -->
+    
+    <!-- page-header -->
+    <header class="page-header">
+     
+      <!-- Page Second Navigation -->
+    <nav class="navbar custom-navbar navbar-expand-md navbar-light bg-primary sticky-top">
+        <div class="container">
+            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">                     
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../public/index.php">Stories</a>
+                    </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="#../public/learn.php">Learn Area</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Activities
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="../public/exhibition.php">Exhibitions</a>
+                            <a class="dropdown-item" href="../../gallery.php">Gallery</a>
+                            <div class="dropdown-divider">Join here</div>
+                            <a class="dropdown-item" href="#website/blog/contact-1.php">Join US</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://www.instagram.com/everydaykibera/" target = "blank">instagram</a>
+                    </li>
+                </ul>
+                <div class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a href="#contact.html">Join Us</a>
+                    </li>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+
+
+
+</header>
+    <!-- end of page header -->
