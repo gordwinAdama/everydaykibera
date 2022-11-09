16c20d8f78d2f249568a2e7cc542e9dbd544f480
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2022-11-09 10:19:15 +0300

    Create exhibition.php

---
+++ website/public/exhibition.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with JoeBLog landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Programs - Exhibitions Announcement</title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+    <!-- Bootstrap + JoeBLog main styles -->
+	<link rel="stylesheet" href="assets/css/joeblog.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+    <!-- Page Second Navigation -->
+    
+
+    <!-- End Of Page Second Navigation -->
+
+    <!-- Page Header -->
+    <header class="page-header page-header-mini">
+        <?php include ('includes/header.php'); ?>
+        <h1 class="title">Welcome to our Exhibitoin News!</h1>
+        <ol class="breadcrumb pb-0">
+            
+            <li class="breadcrumb-item active" aria-current="page">Exhibition Calender</li>
+        </ol>
+    </header>
+    <!-- End Of Page Header -->
+
+    <section class="container">
+        <div class="page-container">
+            <div class="page-content">
+                <div class="card">
+                    <div class="card-header pt-0">
+                        <h3 class="card-title mb-4">Exhibition Details - Kibera's 1st Photo Festival</h3>
+                        <div class="blog-media mb-4">
+                            <img src="assets/imgs/education.jpg" alt="" class="w-100">
+                            
+                        </div>  
+                        <small class="small text-muted">
+                            <a href="#" class="text-muted">Graphics by Alpha Odhimabo</a>
+                            <span class="px-2">·</span>
+                            
+                        </small>
+                    </div>
+                    <div class="card-body border-top">
+                        <p class="my-3">Welcome to our exhibition 
+
+Our first photography class graduated Just before the 2019 Covid pandemic. Over the past three years we’ve earnest our skills of visual storytelling capturing some of the momentous occasions happening here and around. From sports, religion, healthcare, arts and fashion to the pandemic and the 2022 Kenyan general elections. In this we’re showcasing for the first time works of 10 photographers we’ve been on the field with.
+
+Kibera is growing at a first pace, with lots of stories to tell. What seems to triumph despite the shot backs from the social community we’re in is the ever; hard working, creative, and above all it’s a changing community. Canvassed by a visual metaphor that transcends localities - just at the backyards of Nairobi business District. Kibera is facing The pressures of big city growth With mass movements and these leads social issues. The exhibition highlight these movements and how the community faced the pandemic and above how has this place changed since we were kids?
+
+We hope you enjoy this first exhibition by photographers from kibera.</p>
+                    </div>
+                                    
+                </div> 
+
+                <h6 class="mt-5 text-center">Learn more about the photographers on our instagram post. <a href= "https://www.instagram.com/everydaykibera/"> Instagram</a></h6>
+                <hr>
+  
+            </div>
+            <!-- Sidebar -->
+            <div class="page-sidebar">
+                <h6 class=" ">Wanna Join Our TEAM!</h6>
+                <a href="javascript:void(0)" class="badge badge-primary m-1">Buy Prints?</a>
+                <a href="javascript:void(0)" class="badge badge-primary m-1">Support our exhibiton program?</a>
+                <a href="javascript:void(0)" class="badge badge-primary m-1">See Untold Home Stories</a>
+                <a href="javascript:void(0)" class="badge badge-primary m-1">Follow us on Instagram</a>
+                <a href="javascript:void(0)" class="badge badge-primary m-1">Be part of our Educators</a>
+         
+    
+                <div class="ad-card d-flex text-center align-items-center justify-content-center mt-4">
+                    <span href="#" class="font-weight-bold">Join. <br>Us</span>
+                </div>
+            </div>
+        </div>
+        <?php include ('../blog/otherstories-1.php') ; ?>
+    </section>
+
+    <!-- Page Footer -->
+    
+    <?php include ('includes/footer.php'); ?>
+    <!-- End of Page Footer -->
+
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- JoeBLog js -->
+    <script src="assets/js/joeblog.js"></script>
+
+</body>
+</html>
