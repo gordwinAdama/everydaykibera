65289b4a2b5a4a6284c5bdd0b23d4d8c9e0841ab
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2022-11-09 10:01:11 +0300

    Create artinkibera.php

---
+++ website/blog/artinkibera.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+   <head>
+      <!-- Meta -->
+      <title>Local Untold Stories</title>
+      <meta charset="UTF-8">
+      <meta name="description" content="Free HTML template">
+      <meta name="keywords" content="HTML, template, free">
+      <meta name="author" content="Nicola Tolin">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <!-- Styles -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+      <link href="vendor/animate/animate.css" rel="stylesheet" type="text/css"/>
+      <link href="vendor/swiper/css/swiper.min.css" rel="stylesheet" type="text/css"/>
+      <link href="css/style.css" rel="stylesheet" type="text/css"/>
+      <style type="text/css">
+      </style>
+   </head>
+   <body>
+   
+ <?php include ('../public/includes/header.php'); ?>
+
+         <div class="container studio">
+         <div class="row justify-content-center">
+            <div class="col-md-8 col-sm-12 wow fadeInDown">
+               
+               <h1>Art In Kibera.</h1>
+
+               <a href="#" class="fa fa-facebook"></a>
+                  <a href="#" class="fa fa-twitter"></a><b>Community<h2>Art Projects Visualising the Communities In Kibera</h2></b>
+
+               <p>Your it to gave life whom as. Favourable dissimilar resolution led for and had. At play much to time four many. Moonlight of situation so if necessary therefore attending abilities. Calling looking enquire up me to in removal. Park fat she nor does play deal our. Procured sex material his offering humanity laughing moderate can. Unreserved had she nay dissimilar admiration interested. Departure performed exquisite rapturous so ye me resources. 
+               </p>
+               <p>Your it to gave life whom as. Favourable dissimilar resolution led for and had. At play much to time four many. Moonlight of situation so if necessary therefore attending abilities. Calling looking enquire up me to in removal. Park fat she nor does play deal our. Procured sex material his offering humanity laughing moderate can. Unreserved had she nay dissimilar admiration interested. Departure performed exquisite rapturous so ye me resources. 
+                  <br><br>
+                  Explain few led parties attacks picture company. On sure fine kept walk am in it. Resolved to in believed desirous unpacked weddings together. Nor off for enjoyed cousins herself. Little our played lively she adieus far sussex. Do theirs others merely at temper it nearer. 
+                  Smiling nothing affixed he carried it clothes calling he no. Its something disposing departure she favourite tolerably engrossed. Truth short folly court why she their balls. Excellence put unaffected reasonable mrs introduced conviction she. Nay particular delightful but unpleasant for uncommonly who. 
+               </p>
+            </div>
+         </div>
+      </div>
+      <div class="container">
+         <div class="row">
+            <div class="col-xl-5 col-md-7 col-sm-12">
+               <img class="img-fluid" src="img/photo1-1.jpg" alt="Photo">
+               <p>Your it to gave life whom as</p>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-xl-5 col-md-7 offset-md-4 col-sm-12 pt-5">
+               <img class="img-fluid" src="img/photo-2.jpg" alt="Photo">
+               <p>Your it to gave life whom as</p>
+            </div>
+         </div>
+      </div>
+      <div class="container studio">
+         <div class="row justify-content-center">
+            <div class="col-md-8 col-sm-12 wow fadeInDown">
+               <p>Your it to gave life whom as. Favourable dissimilar resolution led for and had. At play much to time four many. Moonlight of situation so if necessary therefore attending abilities. Calling looking enquire up me to in removal. Park fat she nor does play deal our. Procured sex material his offering humanity laughing moderate can. Unreserved had she nay dissimilar admiration interested. Departure performed exquisite rapturous so ye me resources. 
+                  <br><br>
+                  Explain few led parties attacks picture company. On sure fine kept walk am in it. Resolved to in believed desirous unpacked weddings together. Nor off for enjoyed cousins herself. Little our played lively she adieus far sussex. Do theirs others merely at temper it nearer. 
+                  Smiling nothing affixed he carried it clothes calling he no. Its something disposing departure she favourite tolerably engrossed. Truth short folly court why she their balls. Excellence put unaffected reasonable mrs introduced conviction she. Nay particular delightful but unpleasant for uncommonly who. 
+               </p>
+            </div>
+         </div>
+      </div>
+      <!-- Works -->
+      <div class="container-fluid works wow fadeInDown">
+         <div class="row justify-content-center">
+            <div class="col-xl-6 col-md-8 col-sm-12">
+               <p class="text-center">
+                  Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi.
+               </p>
+            </div>
+         </div>
+      </div>
+        <div class="container-fluid works wow fadeInDown">
+         <div class="row justify-content-center">
+            <div class="col-xl-6 col-md-8 col-sm-12">
+               <p class="text-center">
+
+                  <p>Share this story </p>      
+                  <a href="#" class="fa fa-facebook"></a>
+                  <a href="#" class="fa fa-twitter"></a>
+                  <h1>Explore More Stories</h1>
+                 
+               </p>
+            </div>
+
+         </div>
+      </div>
+       <?php include ('contactform.php') ; ?>
+      <!-- End Works -->
+       <!-- Swiper -->
+     
+      <!-- End Contact Form -->
+      <?php include ('../public/includes/header.php'); ?>
+      <!-- End Footer -->
+      <!-- Javascript -->
+      <script src="vendor/jquery.min.js"></script>
+      <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+      <script src="vendor/wow/wow.js"></script>
+      <script src="js/global.min.js"></script>
+      <script src="js/script.js"></script>
+      <script>
+         new WOW().init();
+      </script>
+      <!-- Swiper JS -->
+      <script src="vendor/swiper/js/swiper.min.js"></script>
+      <script>
+         var swiper = new Swiper('.swiper-container', {
+           loop: true,
+           loopFillGroupWithBlank: true,
+           pagination: {
+             el: '.swiper-pagination',
+             clickable: true,
+           },
+           breakpoints: {
+         400: {
+           slidesPerView: 1,
+           slidesPerGroup: 1,
+           spaceBetween: 30,
+         },
+         768: {
+           slidesPerView: 2,
+           slidesPerGroup: 1,
+           spaceBetween: 30,
+         },
+         1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween: 30,
+         },
+         },
+           keyboard: {
+               enabled: true,
+               onlyInViewport: false,
+            },
+           navigation: {
+             nextEl: '.swiper-button-next',
+             prevEl: '.swiper-button-prev',
+           },
+         });
+      </script>
+      <!-- End Javascript -->
+   </body>
+</html>
