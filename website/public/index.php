2181827c4583c233774e9a9f34aa1939fc1a3fe1
Gordwn Odhiambo <gordwinodhiambo@gmail.com>
2022-11-09 10:19:52 +0300

    Add files via upload

---
+++ website/public/index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with JoeBLog landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Untold Local Stories</title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+
+	<link rel="stylesheet" href="assets/css/joeblog.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+    <!-- page First Navigation -->
+ 
+    <!-- End Of First Navigation -->
+   <?php include ('includes/header.php'); ?>
+    <div class="container">
+    
+
+
+            <h1>Welcome to our stories!</h1>
+        With this blog, our team of photographers and storytellers are going beyond single photo feature to capture indepth captivating extended stories behinnd the photos they take. As we continue to learn and be skillfull, we would like to also tell and capture comprehensive stories of the people we photograph in the community. This blog aims to highlite more stories that cuts across cultures, communities and generations in Kibera, Nairobi.</p>
+        <hr>
+
+         <div class="row">                       
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/amo-11.jpg" alt="" class="w-100">
+                                    <a href="#../details/storydetails.php" class="badge badge-primary">.</a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Sunday sunrise Photography Walk</h5>    
+                                <small class="small text-muted">January 20 2022 
+                                    <span class="px-2">-</span>
+                                    <a href="https://www.instagram.com/amaury.faltbrown/" target='blank' class="text-muted">Written by Amaury Faltbrown</a>
+                                </small>
+                                <p class="my-2">
+Though it is often described as an undesirable or dangerous place, Nairobi's Kibera slum is a community like any other. At its core, the fabric of Kibera is made up of everyday people going about their lives. On this particular Sunday morning, the streets were buzzing as residents donned their finest clothes for church, street vendors cooked up fresh breakfast and businesspeople set off to work.
+</p>
+                            </div>
+                            
+                            <div class="card-footer p-0 text-center">
+                                <a href="../blog/works.php" class="btn btn-outline-dark btn-sm">READ MORE</a>
+                            </div>                  
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/insta-7.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary">.</a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Artists visual collection of Home</h5> 
+                                <small class="small text-muted">2022 
+                                    <span class="px-2">-</span>
+                                    <a href="#" class="text-muted">written by Bernad Maingi - artist arts360kibera</a>
+                                </small>
+                                <p class="my-2">For the past 22 years, The Women of Concern Annual Awards Luncheon has gathered to highlight the drive towards gender equality and pay tribute to those who embody justice, generosity and compassion in their work and daily lives.</p>
+                            </div>
+                            
+                            <div class="card-footer p-0 text-center">
+                                <a href="../blog/artinkibera.php" class="btn btn-outline-dark btn-sm">READ MORE</a>
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+
+                    <section>
+            <div class="feature-posts">
+                <a href="single-post.html" class="feature-post-item">                       
+                    <span>External Stories</span>
+                </a>
+                <a href="https://www.facebook.com/yemacalif/posts/kibera-models-bring-heatkibera-nairobi-kenya/924611177981362/?paipv=0&eav=AfYlJ_pY74s82lL6DQDw1uJODhU031OgSHzgW3XG2OUlZMwiDV-JR9carDydtTFHRjY&_rdr" class="feature-post-item">
+                    <img src="assets/imgs/img-1.jpg" class="w-100" alt="">
+                    <div class="feature-post-caption">Annual Yema Fashion - Kibera models bring heat</div>
+                </a>
+                <a href="https://lookeey1.wordpress.com/2022/05/06/i-wish-to-be-a-daddy/" target="blank" class="feature-post-item">
+                    <img src="assets/imgs/uppa-6.jpg" class="w-100" alt="">
+                    <div class="feature-post-caption">I WISH TO BE A DADDY</div>
+                </a>
+                <a href="https://tagree.de/life-in-kibera-on-the-outskirts-of-nairobi-by-samson-otieno/" target ="blank" class="feature-post-item">
+                    <img src="assets/imgs/img-3.jpg" class="w-100" alt="">
+                    <div class="feature-post-caption">Life in Kibera</div>
+                </a>
+               
+            </div>
+        </section>
+         <div class="page-container">
+            <div class="page-content">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h5 class="card-title">Soccer Club Manifestos — The Words Uniting Soccer Lovers in Kibera Soccer</h5> 
+                        <small class="small text-muted">Friday 24th Sept. 2022 
+                            <span class="px-2">-</span>
+                            <a href="../blog/health.php" class="text-muted">Words by Gordwin Odhiambo</a>
+                        </small>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="blog-media">
+                            <img src="assets/imgs/blog-6.jpg" alt="" class="w-100">
+                            <a href="https://tagree.de/goal-by-gordwin-odhiambo/" target = "blank"  class="badge badge-primary">Feature Article</a>     
+                        </div>  
+                        <p class="my-3">Football is said to have the power to bring people together, regardless of their age, race, gender or nationality.
+#Team kibera united👌
+#Har Har💥</p>
+                    </div>
+                    
+                    <div class="card-footer d-flex justify-content-between align-items-center flex-basis-0">
+                        <button class="btn btn-primary circle-35 mr-4"><i class="ti-back-right"></i></button>
+                        <a href="https://tagree.de/goal-by-gordwin-odhiambo/" target = "blank" class="btn btn-outline-dark btn-sm">READ MORE</a>
+                        <a href="../blog/health.php" class="text-dark small text-muted">By : Everyday Kibera</a>
+                    </div>                  
+                </div>
+                <hr>
+                <div class="row">                       
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/blog-2.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary">Read</a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">How Fashion Companies Are Contributing Towards The Fight Against COVID-19</h5>    
+                                <small class="small text-muted">January 20 2022 
+                                    <span class="px-2">-</span>
+                                    <a href="#" class="text-muted">written by</a>
+                                </small>
+                                <p class="my-2">With the Corona Virus Pandemic everywhere, government and top organizations have been contributing resources to put an end to this uncontrollable disease. Fashion companies such as Gucci, Versace, Prada and many more have all put hands together to fight against this disease. As everyone is helping in different form, some have provided medical equipment and Face Masks to help out.
+
+ </p>
+                            </div>
+                            
+                            <div class="card-footer p-0 text-center">
+                                <a href="../blog/health.php" class="btn btn-outline-dark btn-sm">READ MORE</a>
+                            </div>                  
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/railaa.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary">read</a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Kibera Opens it's First Level 3 Hospital</h5> 
+                                <small class="small text-muted">2022 
+                                    <span class="px-2">-</span>
+                                    <a href="#" class="text-muted">Feature by Gordwin Odhiambo</a>
+                                </small>
+                                <p class="my-2">President Uhuru Kenyatta today officially opened the Nairobi Metropolitan Services (NMS) Kibra Level 3 Hospital in Kibera, Nairobi County.
+
+The Head of State was accompanied by former Prime Minister Raila Odinga and NMS Director-General, Lt. Gen. Mohamed Badi.
+
+Uhuru launched two hospitals, both of them Level 3. He said the new facilities will help thousands and ease congestion at Mbagathi Hospital.</p>
+                            </div>
+                            
+                            <div class="card-footer p-0 text-center">
+                                <a href="../blog/health.php" class="btn btn-outline-dark btn-sm">READ MORE</a>
+                            </div>                  
+                        </div>
+                    </div>
+                </div>
+
+
+    <!-- Page Footer -->
+        <!-- Page Footer -->
+    <?php include ('../blog/otherstories-1.php') ; ?>
+    <?php include ('includes/footer.php'); ?>
+    <!-- End of Page Footer -->
+    <!-- End of Page Footer -->
+
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- JoeBLog js -->
+    <script src="assets/js/joeblog.js"></script>
+
+</body>
+</html>
---
+++ website/public/learn.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with JoeBLog landing page.">
+    <meta name="author" content="Devcrud">
+    <title>Learn Area - Everydayk ibera</title>
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+    <!-- Bootstrap + JoeBLog main styles -->
+	<link rel="stylesheet" href="assets/css/joeblog.css">
+</head>
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+    <!-- page First Navigation -->
+  
+    <!-- End Of First Navigation -->
+
+    <!-- Page Second Navigation -->
+
+    <!-- End Of Page Second Navigation -->
+    
+    <!-- page-header -->
+    <?php include ('includes/header.php'); ?>
+    <!-- end of page header -->
+
+    <div class="container">
+       
+        <hr>
+        <div class="page-container">
+            <div class="page-content">
+                <div class="card">
+                    <div class="card-header text-center">
+                        <h5 class="card-title">Resources for young photographers</h5> 
+                        <small class="small text-muted"> 
+                            <span class="px-2">-</span>
+                            
+                            <p class="my-3">Take a look at some of the materials we have shared for photographers.</p>
+                        </small>
+                    </div>
+              
+                    
+                    <div class="card-footer d-flex justify-content-between align-items-center flex-basis-0">
+                        <button class="btn btn-primary circle-35 mr-4"><i class="ti-back-right"></i></button>
+                        <a href="https://www.instagram.com/everydaykibera/" target ="blank" class="btn btn-outline-dark btn-sm">Wanna move with us?!</a>
+                       
+                    </div>                  
+                </div>
+                <hr>
+                <div class="row">                       
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/w11.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary"></a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Portfolio Review and Workshops</h5>    
+                                
+                                <p class="my-2">Join our writing classes by clicking the link in bio</p>
+                            </div>
+                            
+                                         
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/w11.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary"></a>        
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Opportunities for young Photographers</h5> 
+                               
+                                <p class="my-2">sign up for email updates.</p>
+                            </div>
+                            
+                                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">                       
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/w11.jpg" alt="" class="w-100">
+                                    <a href="#" class="badge badge-primary"></a>       
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Community Photography Program</h5>   
+                                
+                                <p class="my-2">Wanna learn photography classes? See our instagram for details.</p>
+                            </div>
+                            
+                                            
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="card text-center mb-5">
+                            <div class="card-header p-0">                                   
+                                <div class="blog-media">
+                                    <img src="assets/imgs/w11.jpg" alt="" class="w-100">
+                                    <a href="#https://barazalab.com/" class="badge badge-primary"></a>      
+                                </div>  
+                            </div>
+                            <div class="card-body px-0">
+                                <h5 class="card-title mb-2">Photography Talks at Baraza Media Lab</h5>  
+                               
+                                <p class="my-2">Our photographers regularly attend workshops to network with other professionals in the field.</p>
+                            </div>
+                            
+                                            
+                        </div>
+                    </div>
+                </div>
+         
+                <button class="btn btn-primary btn-block my-4">Join Our Sunday Photography Walks</button>
+                      <div class="card-body">
+                        <div class="blog-media">
+                            <img src="assets/imgs/w11.jpg" alt="" class="w-100">
+                            
+                        </div>  
+                        
+                    </div>
+            </div>
+        </div>
+
+
+    <!-- Page Footer -->
+<?php include ('../blog/otherstories-1.php') ; ?>
+  <?php include ('includes/footer.php'); ?>
+ 
+    <!-- End of Page Footer -->
+    
+	<!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- JoeBLog js -->
+    <script src="assets/js/joeblog.js"></script>
+
+</body>
+</html>
